7cad3cd2d8854400d7de9ce53a98d7dda7758d13
friday <eunbiryung@gmail.com>
2022-09-04 21:30:17 +0900

    가로세로 배치와 Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,29 @@ class MyApp extends StatelessWidget {
       // home: Text('안녕') //텍스트 위젯
       // home: Icon(Icons.star) //아이콘 위젯
       // home: Image.asset('doge.png') //이미지 위젯
-      home: Center(
-        child: Container(width: 50, height: 50, color: Colors.blue),
-      )
+      // home: Center(
+      //   child: Container(width: 50, height: 50, color: Colors.blue),
+      // )
         // 박스 위젯
+
+      // home: Scaffold(
+      //   appBar: AppBar(),
+      //   body: Container(),
+      //   bottomNavigationBar: BottomAppBar(child: Text("Merong")),
+      // )
+
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(Icons.star),
+            Icon(Icons.star),
+            Icon(Icons.star),
+          ]
+
+        )
+      )
     );
   }
 }
